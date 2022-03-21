ab5c1837cef6823db3a7352d07948c2fd93215a9
Raja Nandakumar Raja.Nandakumar@stfc.ac.uk <nraja@heplnv116.pp.rl.ac.uk>
2022-03-21 23:05:05 +0000

    Add flask modules

---
+++ dev/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+from flask import render_template, url_for, send_from_directory
+import json
+# user email ID coming in from the login could be arbitrarily capitalised
+# Manager emails should all be lower case by design of the programme
+with open("/home/local/nraja/ppd/pulshe/ppd_organogram.json") as f:
+    managers = json.load(f)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    user = request.authorization['username'].lower()
+    if isManager():
+        return render_template('manager.html', my_string=user, my_employees=myEmployees())
+    # The user is not a manager - return just the page itself
+    return onePerson(user=user)
+
+@app.route("/<string:user>")
+def onePerson(user):
+    current_user = request.authorization['username'].lower()
+    if user == current_user or isMyEmployee(user):
+        localDir = "/home/local/nraja/ppd/pulshe/training/"
+        fileName = "index.html"
+        return send_from_directory(localDir + user, fileName)
+    else:
+        return "Not authorised - sorry!"
+
+def isManager():
+    current_user = request.authorization['username'].lower()
+    for manager in managers.keys():
+        if current_user == manager:
+            return True
+    return False
+
+def myEmployees():
+    if not isManager(): return []
+    current_user = request.authorization['username'].lower()
+    for manager in managers.keys():
+        if current_user == manager:
+            return managers[manager]
+
+def isMyEmployee(user):
+    if not isManager(): return False
+    current_user = request.authorization['username'].lower()
+    for manager in managers.keys():
+        if current_user == manager:
+            if user in managers[manager]: return True
+    return False
+
+if __name__ == "__main__":
+    app.run()
