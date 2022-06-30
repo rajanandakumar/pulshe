08cd6a06553733b08b26f9e49d2c2a8e914fd2d5
Raja Nandakumar <rajanandakumar@gmail.com>
2022-06-30 10:30:23 +0100

    Better documentation for which column is used

--- configuration.py
+++ configuration.py
@@ -10,15 +10,15 @@ config = {
     "she_status" : 0,# Want only "Live" people
     # SHE trainings
     "she_trainings" : [
-        ("Induction Refresher test", (22, 13), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=228"),
-        ("STFC H&S BiteSize", (27, 19), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=46"),
-        ("Fire test", (23,14), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=210"),
-        ("DSE training test", (24, 15), "https://lmsweb.stfc.ac.uk/moodle/mod/scorm/view.php?id=534"),
-        ("DSE self assessment  test", (25, 16), "https://uk.sheassure.net/stfc/Portal/Create/Portal/3f31848b-ae08-4dd5-970e-8efc4808362c#/information"),
-        ("Man Hand test", (26, 17, 18), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=168"),
-        ("Asbestos Essentials", (28, 20), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=158"),
-        ("Electrical Safety Essentials", (29, 21), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=181"),
-        ("TEST for ALL Mandatory Training", (30,))
+        ("Induction Refresher test", (22, 13), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=228"), # W N
+        ("STFC H&S BiteSize", (27, 19), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=46"), #  AB T
+        ("Fire test", (23,14), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=210"), # X O
+        ("DSE training test", (24, 15), "https://lmsweb.stfc.ac.uk/moodle/mod/scorm/view.php?id=534"), # Y P
+        ("DSE self assessment  test", (25, 16), "https://uk.sheassure.net/stfc/Portal/Create/Portal/3f31848b-ae08-4dd5-970e-8efc4808362c#/information"), # Z Q
+        ("Man Hand test", (26, 17, 18), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=168"), # AA R S
+        ("Asbestos Essentials", (28, 20), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=158"), # AC  U
+        ("Electrical Safety Essentials", (29, 21), "https://lmsweb.stfc.ac.uk/moodle/course/view.php?id=181"), # AD V
+        ("TEST for ALL Mandatory Training", (30,)) # AE
     ],
     "AllTraining" : "TEST for ALL Mandatory Training", # The test for all mandatory training
 
